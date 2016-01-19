82e599c8cb7d68538ae26e5e1fb8349c07e99304
Pedro Cisternas (Work) <pedrocisternasd@gmail.com>
2016-01-19 17:26:59 -0300

    Login funcionando

--- public/js/controllers/controller.js
+++ public/js/controllers/controller.js
@@ -1,4 +1,19 @@
 angular.module('FenrirCtrl', [])
 .controller('LoginCtrl', ['$scope','$http', 'LoginService', function($scope, $http, LoginService) {
-	$scope.caca="caca!!!";
+	$scope.user =  {username: 'admin', password: '123'};
+  	//$scope.message = '';
+  	$scope.submit = function () {
+  		LoginService.login($scope.user).success(function (data, status, headers, config) {
+  			alert(":D");
+			//$window.sessionStorage.token = data.token;
+			//$window.sessionStorage.usuario = data.usuario._id;
+			//$window.sessionStorage.mensaje = "Hola ".concat(data.usuario.nombre? data.usuario.nombre : data.usuario.username);
+	        //$scope.message = 'Welcome';
+	        //$state.go('app.list');
+	    }).error(function (data, status, headers, config) {
+	    	alert(":'(");
+	        //delete $window.sessionStorage.token;
+	        //$scope.message = 'Error: Invalid user or password';
+	    });
+	};
 }]);
